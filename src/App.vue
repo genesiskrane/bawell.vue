a3a65c7f293c5d7f386a60e86b4d99f9466fba72
Genesis Krane <72081107+genesiskrane@users.noreply.github.com>
2025-08-01 17:42:47 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+	{{ hostname }}
+	<router-view></router-view>
+</template>
+
+<script setup>
+const hostname = window.location.hostname;
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <p>Home View <br>
+    goto <router-link to="/admin">Admin</router-link>
+    </p>
+</template>
